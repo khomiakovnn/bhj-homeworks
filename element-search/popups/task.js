40ceeb40e70e12ac3e693bad29e50c12879577a2
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-14 13:14:07 -0400

    Pop-up task done

---
+++ element-search/popups/task.js
@@ -0,0 +1,18 @@
+const windowModal = document.getElementById('modal_main')
+windowModal.className = "modal modal_active"
+
+const windowClose = document.getElementsByClassName('modal__close')
+const windowShow = document.getElementsByClassName('show-success')
+
+for (let index = 0; index < windowClose.length; index++) {
+    windowClose[index].onclick = () => {
+        windowModal.className = "modal";
+        document.getElementById('modal_success').className = "modal";
+    }
+}
+
+for (let index = 0; index < windowShow.length; index++) {
+    windowShow[index].onclick = () => {
+        document.getElementById('modal_success').className = "modal modal_active"
+    }
+}
