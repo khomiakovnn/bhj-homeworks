b1d57be0ed8a491c53e5bfd3ac7c08ece4e8d7f5
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-15 14:19:34 -0400

    mole fix

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,23 +5,21 @@ function getHole(index) {
 for (let index = 1; index < 10; index++) {
     const hole = getHole(index);
     hole.onclick = () => {
+        let deadCount = document.getElementById("dead").textContent;
+        let lostCount = document.getElementById("lost").textContent;
         if (hole.classList.contains( 'hole_has-mole' )) {
-            document.getElementById("dead").textContent = Number(document.getElementById("dead").textContent) + 1
-            if (document.getElementById("dead").textContent == 10) {
+            document.getElementById("dead").textContent = Number(deadCount) + 1
+            if (deadCount == 9) {
                 alert('You win');
-                for (let index = 1; index < 10; index++) {
-                    const hole = getHole(index);
-                    hole.onclick = () => null
-                }
+                document.getElementById("dead").textContent = 0;
+                document.getElementById("lost").textContent = 0;
             }
         } else {
-            document.getElementById("lost").textContent = Number(document.getElementById("lost").textContent) + 1
-            if (document.getElementById("lost").textContent == 5) {
+            document.getElementById("lost").textContent = Number(lostCount) + 1
+            if (lostCount == 4) {
                 alert('Game over');
-                for (let index = 1; index < 10; index++) {
-                    const hole = getHole(index);
-                    hole.onclick = () => null
-                }
+                document.getElementById("dead").textContent = 0;
+                document.getElementById("lost").textContent = 0;
             }
         }
     };
