df3759b1daad6b0d71c6840daaa5d0b8d66d8ca3
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-15 13:39:58 -0400

    cookie fix

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,5 @@
 const img = document.getElementById("cookie")
 const counter = document.getElementById("clicker__counter")
 img.onclick = () => {
-    counter.textContent = Number(counter.textContent) + 1
-    if (counter.textContent % 2 == 0) {
-        img.height /= 1.3;
-        img.width /= 1.3; 
-    } else {
-        img.height *= 1.3;
-        img.width *= 1.3; 
-    }
+    img.width = ++counter.textContent % 2 ? 250 : 200;
 }
