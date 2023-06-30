a23d34c18282cb8ce9899c7e9a1aa404b0600f10
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-30 13:34:46 -0400

    chat fixed

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,25 +1,31 @@
 document.querySelector(".chat-widget__side-text").onclick = () => {
     document.querySelector(".chat-widget").className = "chat-widget chat-widget_active"
 }
+const replies = [
+    "Добрый день, мы ещё не проснулись. Позвоните через 10 лет",
+    "Привет, мы не на связи",
+    "Здравствуйте, у нас выходной",
+]
 const inputBlock = document.getElementById("chat-widget__input")
 inputBlock.addEventListener('keydown', (event) => {
     if (event.key == 'Enter') {
-        const date = new Date();
-        let timer = String(date.getHours()) + ':' + String(date.getMinutes())
-        const messages = document.querySelector( '.chat-widget__messages' )
-        messages.innerHTML += `
-        <div class="message message_client">
-            <div class="message__time">${timer}</div>
-            <div class="message__text">
-            ${inputBlock.value}
+        if (inputBlock.value != "") {
+            let timer = new Date().toTimeString().slice(0,5)
+            const messages = document.querySelector( '.chat-widget__messages' )
+            messages.innerHTML += `
+            <div class="message message_client">
+                <div class="message__time">${timer}</div>
+                <div class="message__text">
+                ${inputBlock.value}
+                </div>
             </div>
-        </div>
-        <div class="message">
-            <div class="message__time">${timer}</div>
-            <div class="message__text">
-            Добрый день, мы ещё не проснулись. Позвоните через 10 лет
+            <div class="message">
+                <div class="message__time">${timer}</div>
+                <div class="message__text">
+                ${replies[Math.floor(Math.random() * replies.length)]}
+                </div>
             </div>
-        </div>
-        `;
+            `;
+        }
     }
 })
