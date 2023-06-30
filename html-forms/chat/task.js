967d4e0c2f611413003d35389dee45b292464bdb
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-30 13:15:39 -0400

    Chat done

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -3,10 +3,23 @@ document.querySelector(".chat-widget__side-text").onclick = () => {
 }
 const inputBlock = document.getElementById("chat-widget__input")
 inputBlock.addEventListener('keydown', (event) => {
-        if (event.key == 'Enter') {
-            const date = new Date();
-            let timer = String(date.getHours()) + ':' + String(date.getMinutes())
-            console.log(timer)
-            console.log(inputBlock.value)
-        }
+    if (event.key == 'Enter') {
+        const date = new Date();
+        let timer = String(date.getHours()) + ':' + String(date.getMinutes())
+        const messages = document.querySelector( '.chat-widget__messages' )
+        messages.innerHTML += `
+        <div class="message message_client">
+            <div class="message__time">${timer}</div>
+            <div class="message__text">
+            ${inputBlock.value}
+            </div>
+        </div>
+        <div class="message">
+            <div class="message__time">${timer}</div>
+            <div class="message__text">
+            Добрый день, мы ещё не проснулись. Позвоните через 10 лет
+            </div>
+        </div>
+        `;
+    }
 })
