a8cf651d00c524b74cc00d0bb7b083a6a61a9482
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-07-04 14:31:52 -0400

    tooltip done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,18 @@
+const tips = Array.from(document.querySelectorAll(".has-tooltip"))
+
+tips.forEach(element => {
+    let divTip = document.createElement('div')
+    divTip.className = "tooltip"
+    divTip.textContent = element.title
+    element.parentElement.insertBefore(divTip, element.nextElementSibling)
+});
+
+tips.forEach(element => {
+    element.onclick = () => {
+        if (element.nextElementSibling.className == 'tooltip') {
+            tips.forEach(elem => {elem.nextElementSibling.className = 'tooltip'})
+            element.nextElementSibling.className = 'tooltip tooltip_active' 
+        } else {element.nextElementSibling.className = 'tooltip'}
+        return false
+    }
+});
