a1aef68c1c9f7b089cbfb387af1bb1e93b815cd8
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-07-07 13:53:54 -0400

    tooltip fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,13 +5,14 @@ tips.forEach(element => {
     divTip.className = "tooltip"
     divTip.textContent = element.title
     element.parentElement.insertBefore(divTip, element.nextElementSibling)
+    divTip.style = `left: ${Math.round(element.getBoundingClientRect().left)}px; top: ${Math.round(element.getBoundingClientRect().bottom)}px`
 });
 
 tips.forEach(element => {
     element.onclick = () => {
         if (element.nextElementSibling.className == 'tooltip') {
             tips.forEach(elem => {elem.nextElementSibling.className = 'tooltip'})
-            element.nextElementSibling.className = 'tooltip tooltip_active' 
+            element.nextElementSibling.className = 'tooltip tooltip_active'
         } else {element.nextElementSibling.className = 'tooltip'}
         return false
     }
