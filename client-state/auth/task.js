e85e28821b0b32adf8b6aebd7d5fc8a5f5454026
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-07-18 15:17:26 -0400

    fixed

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,23 +1,26 @@
-if (localStorage.user_id) {
-    document.getElementById('welcome').className = "welcome welcome_active"
-    document.getElementById('user_id').textContent = localStorage.user_id
-    document.getElementById('signin__form').className = "signin"
+const form = document.getElementById('signin__form')
+const welcomeDiv = document.getElementById('welcome')
+const userIdDiv = document.getElementById('user_id')
+let textContent = localStorage.user_id
+
+if (textContent) {
+    welcomeDiv.className = "welcome welcome_active"
+    userIdDiv.textContent = textContent
+    form.className = "signin"
 }
 
-document.getElementById('signin__form').onsubmit = (event) => {
+form.reset()
+form.onsubmit = (event) => {
     event.preventDefault()
     
     const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.response.success) {
-                document.getElementById('welcome').className = "welcome welcome_active"
-                document.getElementById('user_id').textContent = xhr.response.user_id
-                document.getElementById('signin__form').className = "signin"
-                localStorage.user_id = xhr.response.user_id
-                console.log(localStorage.user_id)
-            } else {alert('Неверный логин/пароль')}
-        }
+    xhr.onload = () => {
+        if (xhr.response.success) {
+            welcomeDiv.className = "welcome welcome_active"
+            userIdDiv.textContent = xhr.response.user_id
+            form.className = "signin"
+            textContent = xhr.response.user_id
+        } else {alert('Неверный логин/пароль')}
     };
 
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,6 +1,6 @@
 const textBlock = document.getElementById('editor')
 
-textBlock.value = localStorage.text
+textBlock.value = localStorage.getItem('text')
 textBlock.onkeydown = () => {
     localStorage.text = textBlock.value
 }
